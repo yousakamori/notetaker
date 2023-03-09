37cd5893cd54889ef3dce26131a48d7e1c67f678
you <you@yunoMacBook-Pro.local>
2023-03-09 22:14:48 +0900

    first commit

---
+++ src/server/api/root.ts
@@ -0,0 +1,18 @@
+import { createTRPCRouter } from "~/server/api/trpc";
+import { exampleRouter } from "~/server/api/routers/example";
+import { topicRouter } from "~/server/api/routers/topic";
+import { noteRouter } from "./routers/note";
+
+/**
+ * This is the primary router for your server.
+ *
+ * All routers added in /api/routers should be manually added here.
+ */
+export const appRouter = createTRPCRouter({
+  example: exampleRouter,
+  topic: topicRouter,
+  note: noteRouter,
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
